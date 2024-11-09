da7f45f6660e12488b42ab47a9f9f5f8b8855a48
DrowningYu <59392939+DrowningYu@users.noreply.github.com>
2024-11-09 16:04:48 +0800

    Update splitShape.jsx

--- DrowningYu/splitShape/splitShape.jsx
+++ DrowningYu/splitShape/splitShape.jsx
@@ -1,6 +1,6 @@
 #include "../utils.jsx"
 
-function applySplitShape(){
+function applySplitShape() {
     var settings = getSettings();
     var selectedComp = getSelectedComp();
 
@@ -9,18 +9,37 @@ function applySplitShape(){
     if (selectedLayers === null) {
         return;
     }
-    
-    if(selectedLayers.length!=1){
+
+    if (selectedLayers.length != 1) {
         alert("只能选中一个图层")
         return;
     }
-    var selLayer=selectedLayers[0];
-    if(selLayer.matchName != "ADBE Vector Layer"){
+    var selLayer = selectedLayers[0];
+    if (selLayer.matchName != "ADBE Vector Layer") {
         alert("只能选中形状图层")
         return;
     }
-    alert("pass");
+
+
+
+
+
+    // 循环遍历内容组中的每个项目，并复制图层
+    for (var i = 1; i <= selLayer.content.numProperties; i++) {
+        // 复制当前图层
+        var newLayer = selLayer.duplicate();
+        newLayer.name = selLayer.name + '_' + selLayer.content.property(i).name;
+        // 仅保留复制图层的第 i 个内容，其余内容删除
+        for (var j = newLayer.content.numProperties; j > 0; j--) {
+            if (j !== i) {
+                newLayer.property("ADBE Root Vectors Group").property(j).remove();
+            }
+        }
+    }
+
+    selLayer.enabled = false;
+
 }
-function helpSplitShape(){
+function helpSplitShape() {
     alert("拆分形状图层");
 }
